1786b845de60433e1ec7c1eab76f955fe3e4ca9f
Roman <rdubinkin@gmail.com>
2020-11-17 01:44:44 -0500

    disableb played out question

--- frontend/my-app/src/components/Question.js
+++ frontend/my-app/src/components/Question.js
@@ -6,14 +6,16 @@ import Button from '@material-ui/core/Button';
 function Question({ price, state, title }) {
   const dispatch = useDispatch();
   const handleClick = async () => {
-    const response = await fetch(`http://localhost:3100/game/${title}`, {
-      method: 'GET'
-    });
-    const data = await response.json();
-    const index = data.question.findIndex((el) => el.price === price);
-    const obj = { question: data.question[index].title, answer: data.question[index].answer };
-    console.log('>>>>>>> object to send is ', obj);
-    dispatch(startGame({ ...obj, title, price }));
+    if (state) {
+      const response = await fetch(`http://localhost:3100/game/${title}`, {
+        method: 'GET'
+      });
+      const data = await response.json();
+      const index = data.question.findIndex((el) => el.price === price);
+      const obj = { question: data.question[index].title, answer: data.question[index].answer };
+      console.log('>>>>>>> object to send is ', obj);
+      dispatch(startGame({ ...obj, title, price }));
+    }
   }
 
   return (
